a2078e1511805b268c286bab7a52fcb5a9bc76a4
Jordan R <jaygerzero@gmail.com>
2019-11-18 14:59:58 -0500

    Operator Precedence

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ console.log(firstName + ' is a ' + age + ' year old ' + job +'. Is he married? '
 
  /**********************
   * Basic Operators
-  */
+  
 var year, yearJohn, yearMark
 now = 2019
 ageJohn = 28;
@@ -79,3 +79,39 @@ console.log(typeof ageJohn);
 console.log(typeof 'Mark is older than John');
 var x;
 console.log(typeof x);
+*/
+
+
+/********************
+ * Operator precendence 
+ */
+
+ var now = 2019;
+ var yearJohn = 1990;
+ var fullAge = 18;
+
+ // Multiple Operators
+ var isFullAge = now - yearJohn >= fullAge; // true 
+ console.log(isFullAge);
+
+// Grouping
+ var ageJohn = now - yearJohn;
+ var ageMark = 35;
+ var average = (ageJohn + ageMark) / 2;
+ console.log(average);
+
+ // Multiple assignments
+ var x, y;
+ x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+ console.log(x, y);
+
+ // More operators
+ x *= 2;
+ console.log(x);
+ x += 10; 
+ console.log(x);
+ x++;
+ console.log(x);
+ x--;
+ console.log(x);
+ 
