b5ad2b98350ba44282feb3d3064ca399aeacccfb
Jordan R <jaygerzero@gmail.com>
2019-11-18 15:40:36 -0500

    Boolean Logic

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ console.log(typeof x);
 
 /**********************
  * If / else Statements
- */
+ 
 
  var firstName = 'John';
  var civilStatus = 'single';
@@ -179,3 +179,22 @@ var heightMark, heightJohn, massMark, massJohn, bmiMark, bmiJohn, higherBmi;
 //  higherBmi = bmiMark >= bmiJohn;
 
 //  console.log("Is Mark's BMI higher than John's?" + ' ' + higherBmi);
+
+*/
+
+/*********************
+ * Boolean Logic
+ */
+
+ var firstName = 'John';
+ var age = 20;
+
+ if(age < 13) {
+   console.log(firstName + ' is a boy.')
+ } else if (age >= 13 && age < 20) {
+   console.log(firstName + ' is a teenager.')
+ } else if (age > 20 && age < 30) {
+   console.log(firstName + ' is a young man.')
+ } else {
+   console.log(firstName + ' is a man.')
+ }
