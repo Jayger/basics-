e72967f4e2d424c419bfd8a5401f283ff362bfb2
Jordan R <jaygerzero@gmail.com>
2019-11-18 15:57:23 -0500

    Ternary and Switch statement

--- script.js
+++ script.js
@@ -184,7 +184,7 @@ var heightMark, heightJohn, massMark, massJohn, bmiMark, bmiJohn, higherBmi;
 
 /*********************
  * Boolean Logic
- */
+ 
 
  var firstName = 'John';
  var age = 20;
@@ -198,3 +198,58 @@ var heightMark, heightJohn, massMark, massJohn, bmiMark, bmiJohn, higherBmi;
  } else {
    console.log(firstName + ' is a man.')
  }
+ */
+
+ /**********************
+  * The Ternary Operator and Switch Statement
+  */
+
+  var firstName = 'John';
+  var age = 17;
+
+  // ternary operator
+  age >= 18 ? console.log(firstName + ' drinks beer.') 
+  : console.log(firstName + ' drinks juice.')
+
+  var drink = age >= 18 ? 'beet' : 'juice'
+  console.log(drink);
+
+  // if(age >= 18) {
+  //   var drink = 'beer'
+  // } else {
+  //   var drink = 'juice'
+  // };
+
+  // Switch Statement
+  var job ='instructor';
+
+  switch(job) {
+      case 'teacher':
+      case 'instructor':
+        console.log(firstName + ' teaches kids how to code');
+        break;
+      case 'driver':
+        console.log(firstName + ' drives an uver in Lisbon');
+        break;
+      case 'designer':
+        console.log(firstName + ' designs beautiful wensties');
+        break;
+      default: 
+        console.log(firstName + ' does something else');
+  }
+
+  switch(true){
+    case age < 13:
+        console.log(firstName + ' is a boy.')
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age > 20 && age < 30:
+        console.log(firstName + ' is a young man.')
+        break;
+    default:
+        console.log(firstName + ' is a man.')
+
+
+  }
