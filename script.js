3b5ff82b6a1ae85f9c1e6745a3ec02750c821d37
Jordan R <jaygerzero@gmail.com>
2019-11-20 14:49:10 -0500

    Objects and Methods

--- script.js
+++ script.js
@@ -431,3 +431,52 @@ console.log(isDesigner);
 console.log(tips, finalValues);
 
 */
+
+/******************************
+ * Objects and properties
+
+
+// Object Literal
+ var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false
+ };
+ console.log(john.firstName);
+ console.log(john['lastName']);
+ var x = 'birthYear';
+ console.log(john[x]);
+
+ john.job = 'designer';
+ john['isMarried'] = true;
+ console.log(john)
+
+ // new Object syntax
+ var jane = new Object();
+ jane.name = 'Jane';
+ jane.birthYear = 1969;
+ jane['lastName'] = 'Smith';
+ console.log(jane);
+*/
+
+/**************************
+ * Objects and methods
+ */
+
+
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1990,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'teacher',
+  isMarried: false,
+  calcAge: function(){
+    this.age = 2019 - this.birthYear;
+  }
+};
+john.calcAge();
+console.log(john);
