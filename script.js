eb70b698fe57cfeb1ec7a90abfa42b2359f84d15
Jordan R <jaygerzero@gmail.com>
2019-11-20 15:36:33 -0500

    Loops and iterations

--- script.js
+++ script.js
@@ -515,8 +515,9 @@ console.log(john);
 
  /************************
   * Loops and iteration
-  */
+  
 
+  // For loop
   for(var i = 0; i <= 20; i++) {
     console.log(i)
   }
@@ -529,6 +530,34 @@ console.log(john);
 
  var john = ['John', 'Smith', 1990, 'designer', false];
 
+ 
  for(var i = 0; i < john.length; i++) {
    console.log(john[i]);
  }
+
+ // while loop
+ var i = 0;
+ while(i < john.length){
+   console.log(john[i]);
+   i++;
+ }
+ 
+
+ // continue and break statements
+ var john = ['John', 'Smith', 1990, 'designer', false];
+
+ for(var i = 0; i < john.length; i++) {
+    if(typeof john[i] !== 'string') continue;
+    console.log(john[i]);  
+ }
+
+ for(var i = 0; i < john.length; i++) {
+    if(typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// Looping Backwards
+for(var i = john.length - 1 ; i >= 0; i--) {
+    console.log(john[i]);
+}
+*/
